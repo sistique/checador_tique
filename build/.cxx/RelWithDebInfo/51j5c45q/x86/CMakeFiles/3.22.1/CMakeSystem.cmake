1c12c6c8432a945d4ac7009422e1fedee5f10e3b
sistique <sistemas.hdez1983@gmail.com>
2025-08-08 13:36:10 -0600

    Se ajusta elemento de aplicacion

--- build/.cxx/RelWithDebInfo/51j5c45q/x86/CMakeFiles/3.22.1/CMakeSystem.cmake
+++ build/.cxx/RelWithDebInfo/51j5c45q/x86/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.8.0-64-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.8.0-65-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-64-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-65-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/mauriciohdz/Android/Sdk/ndk/27.0.12077973/build/cmake/android.toolchain.cmake")
